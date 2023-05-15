b02592ea38d4f407d888b8c8238c4e5e64c937d0
Simratjot Singh <71722127+simratjassal@users.noreply.github.com>
2023-05-15 16:42:36 +0530

    Update SingleChat.js

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "https://talk-a-tive-backend-5s0y.onrender.com"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chat-app-by-simrat.onrender.com"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
